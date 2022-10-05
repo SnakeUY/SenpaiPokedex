9bc93bc71b9d0f4e2e91574aa9d10719bd1310a3
Eze1998 <111812412+Eze1998@users.noreply.github.com>
2022-10-05 20:53:27 -0300

    orderby

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {useState} from "react";
-import PokemonBank from './components/PokemonBank';
+import { OrderByValue } from './components/PokemonBank';
 
 function App() {
   const [state,setState] = useState({
     pokemons:[
-      {id:"025",name:"Pikachu",species:"Electric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"001",name:"Bulbasaur",species:"Grass",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"007",name:"Squirtle",species:"Water",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"004",name:"Charmander",species:"Fire",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"012",name:"Butterfree",species:"Bug",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"304",name:"Aron",species:"Steel",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"132",name:"Ditto",species:"Normal",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"092",name:"Gastly",species:"Ghost",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-      {id:"152",name:"Mew",species:"Psychic",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
-    ], PokemonSelected:null,
+      {id:25,name:"Pikachu",types:["Electric"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:1,name:"Bulbasaur",types:["Grass","Poison"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:7,name:"Squirtle",types:["Water"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:4,name:"Charmander",types:["Fire"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:12,name:"Butterfree",types:["Bug","Flying"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:304,name:"Aron",types:["Steel","Rock"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:132,name:"Ditto",types:["Normal"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:92,name:"Gastly",types:["Ghost","Type"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:152,name:"Mew",types:["Psychic"],weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+    ].sort((a,b)=>a.id-b.id), PokemonSelected:null,
   })
   const selectedpokemon =(id) => {
     setState ({...state,PokemonSelected: state.pokemons[id]})
   console.log(state.PokemonSelected)
 } 
+const [OrderById,setOrderById]=useState (true)
 
 
 
   return (
     <>
     <Navbar/>
-    <PokemonBank
+    {(!OrderById)?""
+    
+    :
+    <OrderByValue 
     pokemon={state.pokemons}
     select={selectedpokemon}
     />
+    }
     </>
   );
 }
--- src/components/PokemonBank.js
+++ src/components/PokemonBank.js
@@ -1,23 +1,56 @@
 import React from "react";
-
-const PokemonBank = ({pokemon,select}) =>{
- return(
-    <div className="container marginsPokemons">
+const OrderByValue= ({pokemon,select}) =>{
+    console.log(pokemon)
+    return(
+        <div className="container marginsPokemons">
     {
         pokemon.map((poke,key)=>{
+            const numero= addLeadingZeros(poke.id,3);
+            return <PokemonBank pokemons={poke} id={key} select={select} numero={numero}></PokemonBank>
+        }
+
+    )}
+</div> )}
+
+const OrderByAlphabetic= ({pokemon,select}) =>{
+    console.log(pokemon)
+    return(
+        <div className="container marginsPokemons">
+    {
+        pokemon.map((poke,key)=>{
+            const numero= addLeadingZeros(poke.id,3);
+            return <PokemonBank pokemons={poke} id={key} select={select} numero={numero}></PokemonBank>
+        }
+
+    )}
+</div> )}
+
+
+const PokemonBank = ({pokemons,id,select,numero}) =>{
+    console.log(pokemons)
         return (
-            <div className={"pokemon-container borderPokemon border"+ poke.species} key={key} onClick={()=> select(key)}>
+            <div className={"pokemon-container borderPokemon border"+ pokemons.types[0]} key={id} onClick={()=> select(id)}>
                 <div className="bodyPokemon">
-                    <div className={"headerPokemon header" + poke.species}>#{poke.id}</div>
+                    <div className={"headerPokemon header" + pokemons.types[0]}>#{numero}</div>
                     <div className="divImagePokemon">
-                        <img src={`/Sprites/Icons/${poke.name}.png`} alt={poke.name} className="imgPokemon"/>
+                        <img src={`/Sprites/Icons/${pokemons.name}.png`} alt={pokemons.name} className="imgPokemon"/>
                     </div>
                 </div>
-                <div className={"footerPokemon footer"+ poke.species}>{poke.name}</div>
+                <div className={"footerPokemon footer"+ pokemons.types[0]}>{pokemons.name}</div>
             </div>
         )}
-    )}
-    </div>
- )   
-}
-export default PokemonBank
+
+function addLeadingZeros(num, totalLength) {
+    if (num < 0) {
+      const withoutMinus = String(num).slice(1);
+      return '-' + withoutMinus.padStart(totalLength, '0');
+    }
+  
+    return String(num).padStart(totalLength, '0');
+  }
+
+
+
+  
+
+export {OrderByValue,PokemonBank,OrderByAlphabetic}
