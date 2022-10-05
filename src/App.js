df3d930b0a44a4c2bf2a604d1d07de9331fab57c
SnakeUY <101599259+SnakeUY@users.noreply.github.com>
2022-10-04 21:13:06 -0300

    CSS Body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import {useState} from "react";
+import PokemonBank from './components/PokemonBank';
 
 function App() {
+  const [state,setState] = useState({
+    pokemons:[
+      {id:"1",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"2",name:"Bulbasaur",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"3",name:"Squirtle",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"4",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"5",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"6",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"7",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"8",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+      {id:"9",name:"Pikachu",species:"Eletric",weight:"6,0",height:"0,4",moves:["Mega-Puch","Pay-Day"],description:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy",HP:"035",ATK:"055",DEF:"040",SATK:"050",SDEF:"050",SPD:"090"},
+    ],
+  })
+
+
+
   return (
+    <>
     <Navbar/>
+    <PokemonBank
+    pokemon={state.pokemons}
+    />
+    </>
   );
 }
 
---
+++ src/components/PokemonBank.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PokemonBank = ({pokemon}) =>{
+ return(
+    <div className="container margins">
+    {
+        pokemon.map((poke,key)=>{
+        return (
+            <div className={"pokemon-container border"+ poke.species} key={key}>
+                <div className="bodyPokemon">
+                    <div className={"headerPokemon header" + poke.species}>#{poke.id}</div>
+                    <div className="divImagePokemon">
+                        <img src={`/Sprites/Icons/${poke.name}.png`} alt={poke.name} className="imgPokemon"/>
+                    </div>
+                </div>
+                <div className={"footerPokemon footer"+ poke.species}>{poke.name}</div>
+            </div>
+        )}
+    )}
+    </div>
+ )   
+}
+export default PokemonBank
